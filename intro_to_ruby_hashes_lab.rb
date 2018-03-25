a15dd9e28bdbe3a0e566ae9b2f12d7b137bba162
Megan Collier <meganraec@live.com>
2018-03-25 21:45:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,9 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {}
-				:pennsylvania_railroad => {}
-				:b_and_o_railroad => {}
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
 				:shortline_railroad => {}
 			},
 			:rent_in_dollars => {
