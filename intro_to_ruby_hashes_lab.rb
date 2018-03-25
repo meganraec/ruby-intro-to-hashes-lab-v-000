a5e4c6bd60fe4e6f9a0913c0edc753f521796723
Megan Collier <meganraec@live.com>
2018-03-25 21:43:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,13 +21,20 @@ end
 def monopoly_with_third_tier
 	monopoly_with_third_tier = {
 		:railroads => {
-			:pieces => 4, :names => {}, :rent_in_dollars => {}
-			}
+			:pieces => 4, 
+			:names => {
+				:reading_railroad => {}
+				:pennsylvania_railroad => {}
+				:b_and_o_railroad => {}
+				:shortline_railroad => {}
+			}, 
+			:rent_in_dollars => {
+      	:one_piece_owned=>25,
+      	:two_pieces_owned=>50,
+      	:three_pieces_owned=>100,
+      	:four_pieces_owned=>200
+    },
 	}
-	monopoly_with_third_tier[:names][:reading_railroad] = {}
-	monopoly_with_third_tier[:names][:pennsylvania_railroad] = {}
-	monopoly_with_third_tier[:names][:b_and_o_railroad] = {}
-	monopoly_with_third_tier[:names][:shortline_railroad] = {}
 }
 end
 
