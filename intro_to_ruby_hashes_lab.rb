b880c54d9eba1011df37c93465e40d19fcc323f9
Megan Collier <meganraec@live.com>
2018-03-25 21:47:26 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,9 @@ def monopoly_with_fourth_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {},
+				:reading_railroad => {
+					"mortgage_value" => "$100", "mortgage_value" => "$200", "mortgage_value" => "$400", "mortgage_value" => "$800"
+				},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
