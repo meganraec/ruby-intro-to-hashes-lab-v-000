6ca52c32fef039607f8e7cd84dfc99ffe6f3f991
Megan Collier <meganraec@live.com>
2018-03-25 21:22:38 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly_with_second_tier
 		monopoly_with_second_tier = {
 			:railroads => {
-				:pisces => 4, :names => {}, :rent_in_dollars => {}
+				:pieces => 4, :names => {}, :rent_in_dollars => {}
 				}
 		}
 end
